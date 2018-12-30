635543d915534857a9aa24103d987d65bb7b8fdf
Zac Crawford <zwcrawford@gmail.com>
2018-12-30 12:09:07 -0600

    added formBuilder to main.js and created eventListener.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
 entryForm.formBuilder();
 
-let entrySaveBtn = document.getElementById("journalSave")
-entrySaveBtn
+let entrySaveBtn = document.getElementById("journalSave");
+entrySaveBtn.addEventListener("click", eventListener.)//Some var here
